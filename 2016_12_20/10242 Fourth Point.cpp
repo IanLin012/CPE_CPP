8fd0d5a6e65c1e7788520edcb7e0b567b0bd122d
012 <dr.linian@gmail.com>
2022-12-28 10:25:52 +0800

    first commit

--- 2016_12_20/10242 Fourth Point.cpp
+++ 2016_12_20/10242 Fourth Point.cpp
@@ -6,23 +6,23 @@ int main()
 	scanf("%f %f %f %f %f %f %f %f", &x[0],&y[0],&x[1],&y[1],&x[2],&y[2],&x[4],&y[4]);
 	while(1)
 	{
-		x=x[0];
-		y=y[0];
+		float a=x[0];
+		float b=y[0];
 		
 		for(int i=1;i<4;i++)
 		{
-			x += x[i];
-			y += y[i];
+			a += x[i];
+			b += y[i];
 		}
 		
-		for(i=0;i<4;i++)
+		for(int i=0;i<4;i++)
 		{
 			for(int j=i+1;j<4;j++)
 			{
 				if(x[i]==x[j] && y[j]==y[j])
 				{
-					printf("%f ", x - 3*x[i]);
-					printf("%f\n", y - 3*y[i]);
+					printf("%f ", a - 3*x[i]);
+					printf("%f\n", b - 3*y[i]);
 				}
 			}
 		}
--- 2017_03_28/10038 Jolly Jumpers.cpp
+++ 2017_03_28/10038 Jolly Jumpers.cpp
@@ -1,13 +1,28 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <math.h>
 
-using namespace std;
+#define MAX 3000
 
 int main()
 {
     int n;
-    while(cin >> n)
+    while(scanf("%d", &n) != EOF)
     {
-    	int data[3000] = {0};
-        for
+        int key = 1;
+        int data, lastdata;
+        int check[MAX] = {0};
+        for(int i = 0 ; i < n ; i++)
+        {
+            scanf("%d", &data);
+            if(i && key)
+            {
+                int differernce = abs(data - lastdata);
+                if(differernce < 1 || differernce > (n-1) || check[differernce] > 0)
+                key = 0;
+                check[differernce]++;
+            }
+            lastdata = data;
+        }
+        printf("%s\n", key? "Jolly" : "Not jolly");
     }
 }

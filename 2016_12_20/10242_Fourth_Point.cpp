f26cbe42dd9d52a8a16ad67dc7f9648ba7977789
012 <dr.linian@gmail.com>
2022-12-28 15:40:48 +0800

    first commit

--- 2016_12_20/10242_Fourth_Point.cpp
+++ 2016_12_20/10242_Fourth_Point.cpp
@@ -1,29 +1,24 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main()
 {
-	float x[4],y[4];
-	scanf("%f %f %f %f %f %f %f %f", &x[0],&y[0],&x[1],&y[1],&x[2],&y[2],&x[4],&y[4]);
-	while(1)
+	double x[4], y[4];
+	while(cin >> x[0] >> y[0] >> x[1] >> y[1] >> x[2] >> y[2] >> x[3] >> y[3])
 	{
-		float a=x[0];
-		float b=y[0];
-		
-		for(int i=1;i<4;i++)
+		double a=0, b=0;
+		for(int i=0 ; i<4 ; i++)
 		{
 			a += x[i];
 			b += y[i];
 		}
-		
-		for(int i=0;i<4;i++)
+		for(int i=0 ; i<4 ; i++)
 		{
-			for(int j=i+1;j<4;j++)
+			for(int j=i+1 ; j<4 ; j++)
 			{
-				if(x[i]==x[j] && y[j]==y[j])
-				{
-					printf("%f ", a - 3*x[i]);
-					printf("%f\n", b - 3*y[i]);
-				}
+				if(x[i]==x[j] && y[i]==y[j])
+					printf("%.3f %.3f\n", a-x[i]*3, b-y[i]*3);
 			}
 		}
 	}

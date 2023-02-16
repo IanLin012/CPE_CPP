d4c125ce917aa312773513410f831db81d3d95dd
012 <dr.linian@gmail.com>
2023-02-16 16:20:17 +0800

    first commit

--- 2011_05_25/272_TEX_Quotes.cpp
+++ 2011_05_25/272_TEX_Quotes.cpp
@@ -5,11 +5,25 @@ using namespace std;
 
 int main()
 {
-    string s;
-    char sign = '"'
-    while(cin >> s)
+    string text;
+    int count = 0; // 計算引號出現次數
+    while(getline(cin, text)) // 一次讀取一行
     {
-        s = s.replace(s.find('"'), 1)
+        for(int a=0 ; a<text.size() ; a++)
+        {
+            if(text[a]=='"' && count%2!=1)
+            {
+                cout << "``";
+                count++;
+            }
+            else if(text[a]=='"' && count%2==1)
+            {
+                cout << "''";
+                count++;
+            }
+            else cout << text[a];
+            if(a == text.size()-1) cout << endl; // 讀到每行的最後一個字元時換行
+        }
     }
     return 0;
 }

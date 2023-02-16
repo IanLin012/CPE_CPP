4145c64106cb0a3967362de6d511495956274b6f
012 <dr.linian@gmail.com>
2023-02-16 10:40:21 +0800

    first commit

--- 2010_06_09/100_The_3n+1_problem.cpp
+++ 2010_06_09/100_The_3n+1_problem.cpp
@@ -4,49 +4,54 @@
 
 using namespace std;
 
+int func1(long long i, long long j)
+{
+    long long ans = 0;
+    for(; i<=j ; i++)
+    {
+        long long count = 1;
+        long long n = i;
+        while(n != 1)
+        {
+            if(n%2 == 1)
+                n = 3*n + 1;
+            else
+                n /= 2;
+            count++;
+        }
+        ans = max(ans, count);
+    }
+    cout << ans << endl;
+}
+
+int func2(long long i, long long j)
+{
+    long long ans = 0;
+    for(; j<=i ; j++)
+    {
+        long long count = 1;
+        long long n = j;
+        while(n != 1)
+        {
+            if(n%2 == 1)
+                n = 3*n + 1;
+            else
+                n /= 2;
+            count++;
+        }
+        ans = max(ans, count);
+    }
+    cout << ans << endl;
+}
+
 int main()
 {
     long long i, j;
     while(cin >> i >> j)
     {
         cout << i << " " << j << " ";
-        long long ans = 0;
-        if(i <= j)
-        {
-            for(; i<=j ; i++)
-            {
-                long long count = 1;
-                long long n = i;
-                while(n != 1)
-                {
-                    if(n%2 == 1)
-                        n = 3*n + 1;
-                    else
-                        n /= 2;
-                    count++;
-                }
-                ans = max(ans, count);
-            }
-            cout << ans << endl;
-        }
-        else if(i > j)
-        {
-            for(; j<=i ; j++)
-            {
-                long long count = 1;
-                long long n = j;
-                while(n != 1)
-                {
-                    if(n%2 == 1)
-                        n = 3*n + 1;
-                    else
-                        n /= 2;
-                    count++;
-                }
-                ans = max(ans, count);
-            }
-            cout << ans << endl;
-        }
+        if(i <= j) func1(i, j);
+        else if(i > j) func2(i, j);
     }
     return 0;
 }

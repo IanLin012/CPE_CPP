f41c5163955d75897502473b8973a6cf4c696469
012 <dr.linian@gmail.com>
2023-05-23 02:15:51 +0800

    CPE practice

---
+++ 10783_Odd_Sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, a, b;
+    cin >> T;
+    for(int Case=1; Case<=T; Case++) {
+        cin >> a >> b;
+        int ans = 0;
+        for(int i=a; i<=b; i++) {
+            if(i%2 == 1) ans += i;
+        }
+        printf("Case %d: %d\n", Case, ans);
+    }
+    return 0;
+}
